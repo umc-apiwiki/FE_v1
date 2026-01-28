de7005b8cf45b6a0dc2927dff7a9f1d8e7ca5b99
현영 <hyun0@mail.ulsan.ac.kr>
2026-01-28 20:48:17 +0900

    chore: 불필요한 import 수정

--- src/components/APIDetail/PricingSection.tsx
+++ src/components/APIDetail/PricingSection.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export default function PricingSection() {
   return <div>PricingSection</div>
 }
--- src/components/APIDetail/ReviewSection.tsx
+++ src/components/APIDetail/ReviewSection.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export default function ReviewSection() {
   return <div>ReviewSection</div>
 }
